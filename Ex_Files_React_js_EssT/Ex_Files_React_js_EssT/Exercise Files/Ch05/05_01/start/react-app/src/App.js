d08314fca10874fc5b5e28b1692eef1d7cb18de1
Jeremy Taraba <jeremytaraba@gmail.com>
2024-03-18 18:31:05 -0700

    ch5 finished

--- Ex_Files_React_js_EssT/Ex_Files_React_js_EssT/Exercise Files/Ch05/05_01/start/react-app/src/App.js
+++ Ex_Files_React_js_EssT/Ex_Files_React_js_EssT/Exercise Files/Ch05/05_01/start/react-app/src/App.js
@@ -1,22 +1,25 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useRef } from "react";
 
 function App() {
-  const [checked, setChecked] = useReducer(
-    (checked) => !checked,
-    false
-  );
+  const textTitle = useRef()
+  const hexColor = useRef()
+  function submit (e) {
+    e.preventDefault()
+    let title = textTitle.current.value
+    let color = hexColor.current.value
+    alert(`${title}, ${color}`)
+    textTitle.current.value = ""
+    hexColor.current.value = ""
+  }
+  
   return (
-    <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
-    </div>
+    <form onSubmit={submit}>
+      <input ref={textTitle} type="text" placeholder="color...">
+      </input>
+      <input ref={hexColor} type="color"></input>
+      <button>ADD</button>
+    </form>
   );
 }
 
